d04b9149fc32e7f6c8ead5ef3b2849ff2cefa578
Tarxos <ftarxos@gmail.com>
2021-10-30 00:01:59 +0300

    create project for handle task

---
+++ OtherThemes/Lesson4_StructPatterns/ParseLoader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParseLoader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
