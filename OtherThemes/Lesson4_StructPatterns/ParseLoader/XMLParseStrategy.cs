605acbc71c70ad8fcabf7c6b7cd0f08164fa3caa
Tarxos <ftarxos@gmail.com>
2021-11-01 02:07:50 +0300

    start prototyping xml serializer

---
+++ OtherThemes/Lesson4_StructPatterns/ParseLoader/XMLParseStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace ParseLoader
+{
+    class XMLParseStrategy : ParseStrategy
+    {
+        public override IEnumerable<Data> Parse(string data)
+        {
+            using var reader = new StringReader(data);
+            XmlReader serializer = XmlReader.Create(reader);
+            serializer.MoveToContent();
+            serializer.ReadStartElement();
+
+            // deserialize nodes
+            // return data node by node
+
+            return Array.Empty<Data>();
+        }
+    }
+}
