d1fed5d4957defad77a8f49b45f9957b8222dc0b
Tarxos <ftarxos@gmail.com>
2021-11-02 02:11:51 +0300

    implement junk xml serializer

--- OtherThemes/Lesson4_StructPatterns/ParseLoader/XMLParseStrategy.cs
+++ OtherThemes/Lesson4_StructPatterns/ParseLoader/XMLParseStrategy.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Xml;
 
 namespace ParseLoader
@@ -10,14 +12,45 @@ namespace ParseLoader
         public override IEnumerable<Data> Parse(string data)
         {
             using var reader = new StringReader(data);
-            XmlReader serializer = XmlReader.Create(reader);
-            serializer.MoveToContent();
-            serializer.ReadStartElement();
+            XmlDocument serializer = new XmlDocument();
+            serializer.Load(reader);
 
-            // deserialize nodes
-            // return data node by node
+            if (!serializer.HasChildNodes || serializer.ChildNodes.Count != 2) return Array.Empty<Data>();
+            var nodes = serializer.ChildNodes[1].ChildNodes;
 
-            return Array.Empty<Data>();
+            var result = new List<Data>();
+
+            for (var i = 0; i < nodes.Count; i++)
+            {
+                var node = nodes[i];
+                var properties = node.ChildNodes;
+                Data readData = new Data();
+                foreach (XmlNode property in properties)
+                {
+                    if (property.Name == nameof(Data.Name))
+                    {
+                        readData.Name = property.InnerText;
+                    }
+
+                    if (property.Name == nameof(Data.Width) && double.TryParse(property.InnerText, NumberStyles.AllowDecimalPoint, new NumberFormatInfo(), out var width))
+                    {
+                        readData.Width = width;
+                    }
+
+                    if (property.Name == nameof(Data.Height) && double.TryParse(property.InnerText, NumberStyles.AllowDecimalPoint, new NumberFormatInfo(), out var height))
+                    {
+                        readData.Height = height;
+                    }
+
+                    if (property.Name == nameof(Data.Price) && decimal.TryParse(property.InnerText, NumberStyles.AllowDecimalPoint, new NumberFormatInfo(), out var price))
+                    {
+                        readData.Price = price;
+                    }
+                }
+                result.Add(readData);
+            }
+
+            return result;
         }
     }
 }
