b26cb0bf4e5f303a6162503c991f6eed8952bdeb
Tarxos <ftarxos@gmail.com>
2021-11-08 03:04:43 +0300

    prototype usage of scan spam libraries

--- OtherThemes/Lesson5_BehavioralPatterns/ActualUserOfScanSpamDevice/Program.cs
+++ OtherThemes/Lesson5_BehavioralPatterns/ActualUserOfScanSpamDevice/Program.cs
@@ -1,3 +1,20 @@
 ﻿using System;
+using System.Text;
+using ScannerSpammerDevice;
+using ScannerSpammerDevice_User;
 
-Console.WriteLine();
+IScanSpamDeviceHandler deviceMonitor = new ScanSpamDeviceUsingHandler(new ScanSpamDevice());
+deviceMonitor
+    .ConfigureReader().SaveDataWith(new ConsoleSaveStrategy()).SetBufferSize(60)
+    .ConfigureLogger().UseConsole();
+deviceMonitor.ReadFile("File.txt");
+
+
+class ConsoleSaveStrategy : IDataSaveStrategy
+{
+    public DataSaveResult SaveData(byte[] data)
+    {
+        Console.WriteLine(Encoding.UTF8.GetString(data));
+        return new DataSaveResult();
+    }
+}
