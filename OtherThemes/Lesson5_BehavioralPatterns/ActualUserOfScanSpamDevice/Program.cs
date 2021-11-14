b03e8c3e88ddc14463700269bfddbdffdbbc5cc8
Tarxos <ftarxos@gmail.com>
2021-11-15 00:01:26 +0300

    fix parsing, rework data to parse !!! still place for refactoring

--- OtherThemes/Lesson5_BehavioralPatterns/ActualUserOfScanSpamDevice/Program.cs
+++ OtherThemes/Lesson5_BehavioralPatterns/ActualUserOfScanSpamDevice/Program.cs
@@ -13,13 +13,14 @@ string fileContent = @"Image Title=""Spaghetti"";Content=""h4i3p2kn43on34no34in3
 
 Note Title=""Eat"";Body=""Try Eat Something"";
 
-Note Title=""Eat"";Body=""Try Eat Something"";
+Note Title=""Write a note"";Body=""Custom not description"";
 
-Note Title=""Eat"";Body=""Try Eat Something"";
+Note Title=""Read all text"";Body=""Read all notes"";
 
-Image Title=""Bread"";Content=""h4i3p2kn43on34no34in34ni6j56i4j7j57o45j5l23l3l5j2lj636l34j6"";
+Image Title=""Bread"";Content=""h4i3p328h09igo23hvo9jw9340vijv48303hbrj84jg9vj4iv43b34ib43jb0934jg436l34j6"";
 
-Image Title=""Eggs"";Content=""h4i3p2kn43on34no34in34ni6j56i4j7j57o45j5l23l3l5j2lj636l34j6"";
+Image Title=""Eggs"";Content=""h4i3p2kn43on34no34in34ni6j56i4j7onbe9wpogjj84j09vkmv32iu4b39jv2ova8ugl4hvkk84gbjleb349jbn349b4j3b8943bier8943bhefuidbhr9pegjl43hj57o45j5l23l3l5j2lj636l34j6"";
+
 ";
 
 File.WriteAllText(filePath, fileContent, Encoding.UTF8);
@@ -61,24 +62,23 @@ class ImageParseHandler : ParseHandler
     public override (bool CanParse, bool EnoughDataToParse) CanParse(byte[] data)
     {
         var temp = Encoding.UTF8.GetString(data);
-        if (temp.StartsWith("Image") || temp.StartsWith("\r\nImage"))
-        {
-            var lastLineBreakIndex = temp.LastIndexOf("\r\n\r\n", Comparison);
-            return (true, lastLineBreakIndex > 0 && lastLineBreakIndex <= temp.Length);
-        }
-        return (false, false);
+        var markBlock = temp[..(temp.Length > 40 ? 40 : temp.Length)];
+
+        if (!markBlock.Contains("Image")) return (false, false);
+
+        var lastLineBreakIndex = temp.LastIndexOf("\r\n\r\n", Comparison);
+        return (true, lastLineBreakIndex > 0 && lastLineBreakIndex <= temp.Length);
     }
 
     public override object Parse(byte[] data, out int parsedDataSize)
     {
-        
         var temp = Encoding.UTF8.GetString(data).AsSpan();
 
         var start = temp.IndexOf("Image", Comparison);
-        var tempBlock = temp.Slice(start, temp.Length - 1);
-        var end = tempBlock.IndexOf("\r\n\r\n");
+        var end = temp.IndexOf("\r\n\r\n") - 1;
+        var tempBlock = temp.Slice(start, end);
 
-        parsedDataSize = end + 3;
+        parsedDataSize = Encoding.UTF8.GetBytes(temp[..(end+start)].ToString()).Length + 4;
 
         var imageBlock = tempBlock[..end];
         var imageDataBlock = imageBlock.ToString().Split(" ").LastOrDefault();
@@ -108,17 +108,40 @@ class NoteParseHandler : ParseHandler
     public override (bool CanParse, bool EnoughDataToParse) CanParse(byte[] data)
     {
         var temp = Encoding.UTF8.GetString(data);
-        if (temp.StartsWith("Note") || temp.StartsWith("\r\nNote"))
-        {
-            var lastLineBreakIndex = temp.LastIndexOf("\r\n\r\n", Comparison);
-            return (true, lastLineBreakIndex > 0 && lastLineBreakIndex <= temp.Length);
-        }
-        return (false, false);
+        var markBlock = temp[..(temp.Length > 40 ? 40 : temp.Length)];
+
+        if (!markBlock.Contains("Note")) return (false, false);
+
+        var lastLineBreakIndex = temp.LastIndexOf("\r\n\r\n", Comparison);
+        return (true, lastLineBreakIndex > 0 && lastLineBreakIndex <= temp.Length);
     }
 
     public override object Parse(byte[] data, out int parsedDataSize)
     {
-        parsedDataSize = 0;
-        return new Note("Not Parsed", "");
+        var temp = Encoding.UTF8.GetString(data).AsSpan();
+
+        var start = temp.IndexOf("Note", Comparison);
+        var end = temp.IndexOf("\r\n\r\n");
+        var tempBlock = temp.Slice(start, end);
+
+        parsedDataSize = Encoding.UTF8.GetBytes(temp[..(end + start)].ToString()).Length + 4;
+
+        var noteBlock = tempBlock[..end];
+        var noteDataBlock = noteBlock[(noteBlock.IndexOf(" ") + 1)..].ToString();
+
+        var noteDataBlocks = noteDataBlock.Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+        Dictionary<string, string> noteData = new();
+
+        foreach (var dataBlock in noteDataBlocks)
+        {
+            var pair = dataBlock.Split("=");
+            noteData.Add(pair[0], pair[1].Trim('"'));
+        }
+
+        string title = noteData["Title"];
+        string body = noteData["Body"];
+
+        return new Note(title, body);
     }
 }
