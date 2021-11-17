9ebc33a91fa668c566c37c150c0e6b277c8fe266
Tarxos <ftarxos@gmail.com>
2021-11-17 03:08:32 +0300

    make base calculator

--- OtherThemes/Lesson6_AutofacAndCalculator/Calculator/CalculatorApp/AutofacExtensions.cs
+++ OtherThemes/Lesson6_AutofacAndCalculator/Calculator/CalculatorApp/AutofacExtensions.cs
@@ -10,5 +10,17 @@ namespace CalculatorApp
             registration?.Invoke(self);
             return self;
         }
+
+        public static ContainerBuilder AddOperation(this ContainerBuilder self, string name, string description, Operation<double> operation)
+        {
+            OperationMetadata metadata = new()
+            {
+                Name = name,
+                Description = description,
+                Operation = operation
+            };
+            self.RegisterInstance(metadata);
+            return self;
+        }
     }
 }
--- OtherThemes/Lesson6_AutofacAndCalculator/Calculator/CalculatorApp/Program.cs
+++ OtherThemes/Lesson6_AutofacAndCalculator/Calculator/CalculatorApp/Program.cs
@@ -1,17 +1,104 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Autofac;
 using CalculatorApp;
+using Sharprompt;
 
 //===================================================
 
 var container = new ContainerBuilder().RegisterMany(builder =>
 {
-
+    builder.RegisterType<Calculator>();
+    builder.AddOperation("Sum", "Summation of two operands", Operations.Sum);
+    builder.AddOperation("Sub", "Subtraction of two operands", Operations.Subtract);
+    builder.AddOperation("Mult", "Multiply two operands", Operations.Multiply);
+    builder.AddOperation("Div", "Division of two operands", Operations.Division);
 }).Build();
 
 //===================================================
-Console.WriteLine();
+
+container.Resolve<Calculator>().Start();
 
 //===================================================
 
+class Calculator
+{
+    private readonly IEnumerable<OperationMetadata> _operations;
+
+    public Calculator(IEnumerable<OperationMetadata> operations) => _operations = operations;
+
+    public void Start()
+    {
+        double? previousResult = null;
+        do
+        {
+            do
+            {
+                var operation = SelectOperation(previousResult);
+                previousResult = operation.Calculate();
+                Console.WriteLine($"Result of operation is {previousResult}");
+            } while (Prompt.Confirm("Continue calculation with previous result?")); 
+        } while (!Prompt.Confirm("You wanna exit?"));
+    }
+
+    private CalculationOperation SelectOperation(double? previousResult = null)
+    {
+        OperationFormModel<double> form;
+        if (previousResult is null)
+        {
+            form = Prompt.AutoForms<OperationFormModel<double>>();
+        }
+        else
+        {
+            var rightOperand = Prompt.Input<double>("Input right operand");
+            form = new OperationFormModel<double>() { LeftOperand = previousResult.Value, RightOperand = rightOperand };
+        }
+        
+        var operation = Prompt.Select("Select operation", _operations);
+        return new(form, operation.Operation);
+    }
+}
+
+
 //===================================================
+
+record CalculationOperation(OperationFormModel<double> Form, Operation<double> Operation)
+{
+    public double Calculate() => Operation.Invoke(Form);
+};
+
+delegate T Operation<T>(OperationFormModel<T> formModel);
+
+class OperationFormModel<T>
+{
+    [Display(Prompt = "Input left operand")]
+    public T LeftOperand { get; set; }
+
+    [Display(Prompt = "Input right operand")]
+    public T RightOperand { get; set; }
+}
+
+static class Operations
+{
+    public static double Sum(OperationFormModel<double> formModel)
+    {
+        return formModel.LeftOperand + formModel.RightOperand;
+    }
+
+    public static double Subtract(OperationFormModel<double> formModel)
+    {
+        return formModel.LeftOperand - formModel.RightOperand;
+    }
+
+    public static double Multiply(OperationFormModel<double> formModel)
+    {
+        return formModel.LeftOperand * formModel.RightOperand;
+    }
+
+    public static double Division(OperationFormModel<double> formModel)
+    {
+        return formModel.LeftOperand / formModel.RightOperand;
+    }
+}
