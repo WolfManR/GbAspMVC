37dc35c15f51242d0894ef25dd7356a592b2e9fd
Tarxos <ftarxos@gmail.com>
2021-11-20 00:20:15 +0300

    prototype mongo save strategy

---
+++ OtherThemes/Lesson6_AutofacAndCalculator/Autofac/ActualUserOfScanSpamDevice/DataSaveStrategies/MongoDbConnection.cs
@@ -0,0 +1,26 @@
+﻿using ActualUserOfScanSpamDevice.DataModels;
+using MongoDB.Driver;
+
+namespace ActualUserOfScanSpamDevice.DataSaveStrategies
+{
+    sealed class MongoDbConnection
+    {
+        private const string ConnectionString = "mongodb://root:pass12345@localhost:27017";
+        private readonly IMongoDatabase _database;
+
+        public MongoDbConnection()
+        {
+            var mongoClient = new MongoClient(ConnectionString);
+            _database = mongoClient.GetDatabase("ScanSpam");
+        }
+
+        public IMongoCollection<Image> Images => GetCollection<Image>(nameof(Image));
+        public IMongoCollection<Note> Notes => GetCollection<Note>(nameof(Note));
+
+        private IMongoCollection<T> GetCollection<T>(string name)
+        {
+            var collection = _database.GetCollection<T>(name.ToLowerInvariant(), new MongoCollectionSettings() { AssignIdOnInsert = true });
+            return collection;
+        }
+    }
+}
---
+++ OtherThemes/Lesson6_AutofacAndCalculator/Autofac/ActualUserOfScanSpamDevice/DataSaveStrategies/MongoDbSaveStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ScannerSpammerDevice_User;
+
+namespace ActualUserOfScanSpamDevice.DataSaveStrategies
+{
+    class MongoDbSaveStrategy : IDataSaveStrategy
+    {
+        private readonly Dictionary<Type, SaveOperation> _saveOperations;
+        
+        public MongoDbSaveStrategy(IEnumerable<SaveOperation> saveOperations)
+        {
+            _saveOperations = saveOperations.Where(o=>o.SaveDirection == SaveDirection.Mongo).ToDictionary(o => o.OperationType);
+        }
+
+        public DataSaveResult SaveData(object data)
+        {
+            if (!_saveOperations.TryGetValue(data.GetType(), out var saveOperation)) return new DataSaveResult(false);
+            saveOperation.Save(data);
+            return new DataSaveResult();
+        }
+    }
+}
