af2c54c982600140c2ea4d901bff30bdf495a54f
Tarxos <ftarxos@gmail.com>
2021-11-22 03:07:51 +0300

    read data from csv

--- OtherThemes/Lesson7_Reports/Reporter/Program.cs
+++ OtherThemes/Lesson7_Reports/Reporter/Program.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Data;
+using System.Globalization;
 using System.IO;
 using System.Security.Policy;
+using CsvHelper;
+using CsvHelper.Configuration;
 using FastReport;
 using FastReport.Export.PdfSimple;
 using static System.Console;
+
+CsvConfiguration csvConfiguration = new(CultureInfo.InvariantCulture)
+{
+    Delimiter = ","
+};
+CsvReader reader = new(File.OpenText(@"Temp\sample-csv-file-for-testing.csv"), csvConfiguration);
+reader.Context.RegisterClassMap<RawProductInfoMap>();
+var records = reader.EnumerateRecordsAsync(new RawProductInfo());
+
+int stopRead = 10;
+int read = 0;
+
+await foreach (var rawData in records)
+{
+    if(read == stopRead) break;
+    read++;
+    Console.WriteLine(rawData);
+}
 
 //var directory = @"D:\demo\test";
 
@@ -33,4 +54,38 @@ static class Extensions
 {
     public static string MergePath(this string self, string endPath) => Path.Combine(self, endPath);
     public static string MergePath(this string self, string middlePath, string endPath) => Path.Combine(self, middlePath, endPath);
+}
+// Segment,Country,Product,Discount Band,Units Sold,Manufacturing Price,Sale Price,Gross Sales,Discounts,Sales,COGS,Profit,Date,Month Number,Month Name,Year
+record RawProductInfo
+{
+    public string Segment { get; set; }
+    public string Country { get; set; }
+    public string Product { get; set; }
+    public string DiscountBand { get; set; }
+    public double UnitsSold { get; set; }
+    public string ManufacturingPrice { get; set; }
+    public string SalePrice { get; set; }
+    public string GrossSales { get; set; }
+    public string Discounts { get; set; }
+    public string Sales { get; set; }
+    public string COGS { get; set; }
+    public string Profit { get; set; }
+    public string Date { get; set; }
+    public string MonthNumber { get; set; }
+    public string MonthName { get; set; }
+    public string Year { get; set; }
+}
+
+sealed class RawProductInfoMap : ClassMap<RawProductInfo>
+{
+    public RawProductInfoMap()
+    {
+        Map(m => m.DiscountBand).Name("Discount Band");
+        Map(m => m.UnitsSold).Name("Units Sold");
+        Map(m => m.ManufacturingPrice).Name("Manufacturing Price");
+        Map(m => m.SalePrice).Name("Sale Price");
+        Map(m => m.GrossSales).Name("Gross Sales");
+        Map(m => m.MonthNumber).Name("Month Number");
+        Map(m => m.MonthName).Name("Month Name");
+    }
 }
