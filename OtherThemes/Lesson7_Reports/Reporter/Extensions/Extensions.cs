f844538baa634c2315ca8b4b5cac6e64e399d061
Tarxos <ftarxos@gmail.com>
2021-11-29 03:05:51 +0300

    add Path extensions

---
+++ OtherThemes/Lesson7_Reports/Reporter/Extensions/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace Reporter.Extensions
+{
+    public static class Extensions
+    {
+        public static string MergePath(this string self, string endPath) => Path.Combine(self, endPath);
+        public static string MergePath(this string self, string middlePath, string endPath) => Path.Combine(self, middlePath, endPath);
+    }
+}
