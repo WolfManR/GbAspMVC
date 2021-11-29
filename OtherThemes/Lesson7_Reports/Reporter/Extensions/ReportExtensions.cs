3861194ab89cb0a953e65fdd7d5ce430631a578d
Tarxos <ftarxos@gmail.com>
2021-11-29 03:04:54 +0300

    add extensions for reports

---
+++ OtherThemes/Lesson7_Reports/Reporter/Extensions/ReportExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using FastReport;
+using FastReport.Export.PdfSimple;
+
+namespace Reporter.Extensions
+{
+    public static class ReportExtensions
+    {
+        public static Report SetDataToReportDataBind<TDataModel>(
+            this Report report,
+            IReadOnlyCollection<TDataModel> data,
+            string reportDataSourceName,
+            string reportDataBindName)
+        {
+            report.Dictionary.Clear();
+            report.RegisterData(data, reportDataSourceName);
+
+            var reportDataSource = report.GetDataSource(reportDataSourceName);
+            reportDataSource.Enabled = true;
+
+            DataBand dataBand = (DataBand)report.FindObject(reportDataBindName);
+            dataBand.DataSource = reportDataSource;
+
+            return report;
+        }
+
+        public static Report ExportToPdf(this Report report, string outputPath)
+        {
+            PDFSimpleExport pdfExport = new();
+
+            pdfExport.Export(report, outputPath);
+
+            return report;
+        }
+    }
+}
