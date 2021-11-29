119d7b2f4b92a935b7b1a6c600a6a90907c4d696
Tarxos <ftarxos@gmail.com>
2021-11-29 03:03:00 +0300

    add csv importer

---
+++ OtherThemes/Lesson7_Reports/Reporter/Importers/CsvImporter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Reporter.Importers
+{
+    public class CsvImporter
+    {
+        private readonly CsvConfiguration _csvConfiguration;
+
+        public CsvImporter()
+        {
+            _csvConfiguration = new(CultureInfo.InvariantCulture)
+            {
+                DetectDelimiter = true,
+            };
+        }
+
+        public IAsyncEnumerable<TImportModel> ReadImportData<TImportModel, TImportModelMap>(string filePath)
+            where TImportModelMap : ClassMap
+            where TImportModel : new()
+        {
+            using var stream = File.OpenText(filePath);
+            using CsvReader reader = new(stream, _csvConfiguration);
+
+            reader.Context.RegisterClassMap<TImportModelMap>();
+
+            return reader.EnumerateRecordsAsync(new TImportModel());
+        }
+    }
+}
