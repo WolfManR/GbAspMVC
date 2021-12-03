b817071db0239023e97421b815eac871a7279e53
Tarxos <ftarxos@gmail.com>
2021-12-03 22:03:31 +0300

    add base models

---
+++ OtherThemes/Lesson8_FirstTryASPMVC/OfficeDashboard/Models/Employ.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OfficeDashboard.Models
+{
+    public class Employ
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ OtherThemes/Lesson8_FirstTryASPMVC/OfficeDashboard/Models/Office.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OfficeDashboard.Models
+{
+    public class Office
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Employ> Employees { get; set; } = new HashSet<Employ>();
+    }
+}
