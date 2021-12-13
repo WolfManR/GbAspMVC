34d6544f06cf4dcc31eb9e555a2bc5bc70d161c4
Tarxos <ftarxos@gmail.com>
2021-12-14 01:51:23 +0300

    clear broken code in ModelsGenerator with new viewmodels

--- OtherThemes/Lesson8_FirstTryASPMVC/OfficeDashboard/Services/ModelsGenerator.cs
+++ OtherThemes/Lesson8_FirstTryASPMVC/OfficeDashboard/Services/ModelsGenerator.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Bogus;
-using OfficeDashboard.Models;
+using OfficeDashboard.ViewModels;
 
 namespace OfficeDashboard.Services
 {
@@ -13,20 +13,19 @@ namespace OfficeDashboard.Services
             office.Name = faker.Company.CompanyName();
         });
 
-        private static readonly Faker<Employee> Employees = new Faker<Employee>().Rules((faker, employ) =>
+        private static readonly Faker<ListEmployee> Employees = new Faker<ListEmployee>().Rules((faker, employ) =>
         {
             employ.Id = Guid.NewGuid();
             employ.Name = faker.Person.FirstName;
             employ.Surname = faker.Person.LastName;
         });
 
-        public IEnumerable<Employee> GenerateEmployees(int count = 1) => Employees.Generate(count);
+        public IEnumerable<ListEmployee> GenerateEmployees(int count = 1) => Employees.Generate(count);
         public Office GenerateOffice(int countEmployees = 1)
         {
             var office = Offices.Generate();
             foreach (var employ in GenerateEmployees(countEmployees))
             {
-                employ.Office = office;
                 office.Employees.Add(employ);
             }
             return office;
