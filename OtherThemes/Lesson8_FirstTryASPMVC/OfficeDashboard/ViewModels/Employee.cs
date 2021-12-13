084368cedf875655e3b53c5930599035c136065b
Tarxos <ftarxos@gmail.com>
2021-12-14 01:50:47 +0300

    rename Models folder to ViewModels and add few models for employees and offices

---
+++ OtherThemes/Lesson8_FirstTryASPMVC/OfficeDashboard/ViewModels/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OfficeDashboard.ViewModels
+{
+    public class ListEmployee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+
+    public class CreateEmployee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Guid OfficeId { get; set; }
+    }
+
+    public class EditEmployee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Guid OfficeId { get; set; }
+    }
+}
---
+++ OtherThemes/Lesson8_FirstTryASPMVC/OfficeDashboard/ViewModels/Office.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OfficeDashboard.ViewModels
+{
+    public class Office
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<ListEmployee> Employees { get; set; } = new HashSet<ListEmployee>();
+    }
+
+    public class OfficeSelectListItem
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+    }
+
+    public class EditOffice
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
