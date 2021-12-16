719387c48cb39e960c4721fd831d4a752c7bf5b2
Tarxos <ftarxos@gmail.com>
2021-12-17 01:54:35 +0300

    add base realization of mail content build from razor template

---
+++ src/MailTemplates.Razor/MailContentBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using RazorEngineCore;
+
+namespace MailTemplates.Razor
+{
+    public class MailContentBuilder
+    {
+        private readonly IRazorEngine _razorEngine;
+
+        public MailContentBuilder(IRazorEngine razorEngine)
+        {
+            _razorEngine = razorEngine;
+        }
+
+        public async Task<string> BuildWithTemplate(string template, object model)
+        {
+            var templateCompiler = await _razorEngine.CompileAsync(template);
+
+            var mailContent = await templateCompiler.RunAsync(model);
+
+            return mailContent;
+        }
+    }
+
+    public class MailContentBuilder<TModel>
+    {
+        private readonly IRazorEngine _razorEngine;
+
+        public MailContentBuilder(IRazorEngine razorEngine)
+        {
+            _razorEngine = razorEngine;
+        }
+
+        public async Task<string> BuildWithTemplate(string template, TModel model)
+        {
+            var templateCompiler = await _razorEngine.CompileAsync<RazorEngineTemplateBase<TModel>>(template);
+
+            var mailContent = await templateCompiler.RunAsync(instance => instance.Model = model);
+
+            return mailContent;
+        }
+    }
+}
