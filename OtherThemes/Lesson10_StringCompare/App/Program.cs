ba0b3351fa765fadc7ca6e9c3d4238c16b3f14f0
Tarxos <ftarxos@gmail.com>
2021-12-22 02:55:56 +0300

    create project for string comparing

---
+++ OtherThemes/Lesson10_StringCompare/App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+
+string a = "AABBJJOODD";
+string b = "ABJODABJOD";
+
+WriteLine(Compare(a, b));
+
+bool Compare(string left, string right)
+{
+    Dictionary<char, int> tempCounter = new();
+
+    return false;
+}
