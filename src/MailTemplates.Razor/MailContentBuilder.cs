6697412c4e78cd53aa966220b22c657d66443ead
Tarxos <ftarxos@gmail.com>
2021-12-23 02:12:10 +0300

    add mark in code of MailContentBuilder to wrap it with catching exceptions, and remove generic variant because it's pointless when templating must become more dynamic that strongly typed

--- src/MailTemplates.Razor/MailContentBuilder.cs
+++ src/MailTemplates.Razor/MailContentBuilder.cs
@@ -16,26 +16,8 @@ namespace MailTemplates.Razor
         {
             var templateCompiler = await _razorEngine.CompileAsync(template);
 
+            // it make errors
             var mailContent = await templateCompiler.RunAsync(model);
-
-            return mailContent;
-        }
-    }
-
-    public class MailContentBuilder<TModel>
-    {
-        private readonly IRazorEngine _razorEngine;
-
-        public MailContentBuilder(IRazorEngine razorEngine)
-        {
-            _razorEngine = razorEngine;
-        }
-
-        public async Task<string> BuildWithTemplate(string template, TModel model)
-        {
-            var templateCompiler = await _razorEngine.CompileAsync<RazorEngineTemplateBase<TModel>>(template);
-
-            var mailContent = await templateCompiler.RunAsync(instance => instance.Model = model);
 
             return mailContent;
         }
