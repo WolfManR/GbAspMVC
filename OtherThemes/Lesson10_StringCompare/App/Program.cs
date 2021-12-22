418260ac7a862854551390560c1846c67cb968f0
Tarxos <ftarxos@gmail.com>
2021-12-23 00:50:50 +0300

    write compare function prototype on which was talked in start of lecture

--- OtherThemes/Lesson10_StringCompare/App/Program.cs
+++ OtherThemes/Lesson10_StringCompare/App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using static System.Console;
 
 string a = "AABBJJOODD";
@@ -6,9 +7,27 @@ string b = "ABJODABJOD";
 
 WriteLine(Compare(a, b));
 
+// There still problem with comparing symbols in different unicode tables
 bool Compare(string left, string right)
 {
     Dictionary<char, int> tempCounter = new();
 
-    return false;
+    foreach (var character in left)
+    {
+        if (!tempCounter.ContainsKey(character))
+        {
+            tempCounter.Add(character, 1);
+            continue;
+        }
+        tempCounter[character]++;
+    }
+
+    foreach (var character in right)
+    {
+        if (!tempCounter.ContainsKey(character)) return false;
+        tempCounter[character]--;
+        if (tempCounter[character] < 0) return false;
+    }
+    
+    return !tempCounter.Values.Any(i => i > 0);
 }
