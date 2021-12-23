60a169e9f33ef4ce50a488d371e46cf5d409e90d
Tarxos <ftarxos@gmail.com>
2021-12-24 01:24:12 +0300

    add prototype of template record and add simple repository

---
+++ src/UI/Services/TemplatesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UI.DataModels;
+
+namespace UI.Services
+{
+    public class TemplatesRepository
+    {
+        private static List<TemplateRecord> _templates;
+
+        public TemplatesRepository() => _templates ??= new List<TemplateRecord>();
+
+        public TemplateRecord? Get(string name) => _templates.FirstOrDefault(t => t.Name == name);
+        public void Save(TemplateRecord templateRecord) => _templates.Add(templateRecord);
+    }
+}
