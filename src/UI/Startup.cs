c6f453faf74e36e93323d4bae14e6af10b8a1a40
Tarxos <ftarxos@gmail.com>
2022-01-15 03:27:47 +0300

    injection and realization of Blazor Server Authentication now in working state

--- src/UI/Startup.cs
+++ src/UI/Startup.cs
@@ -1,8 +1,10 @@
+using System;
 using MailSender.MailKit;
 
 using MailTemplates.Razor;
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,15 +31,24 @@ namespace UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // set AddRazorPages and ServerSideBlazor first to override registered by them services
+            services.AddRazorPages();
+            // AddServerSideBlazor register it self AuthenticationStateProvider if it in down of list custom provider will not be used
+            services.AddServerSideBlazor();
+
             var urls = Configuration.GetSection(nameof(Urls)).Get<Urls>();
+
+            // register authentication state provider in that manner to simplify it access
+            services.AddScoped<WebApiAuthenticationStateProvider>();
+            services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<WebApiAuthenticationStateProvider>());
+
+            services.AddHttpClient<AuthenticationService>(client => client.BaseAddress = new Uri(urls.Identity));
 
             services.AddSingleton<IRazorEngine, RazorEngine>();
             services.AddSingleton<ITemplateBuilder, MailContentBuilder>();
             services.AddSingleton<IEmailService, EmailService>();
             services.AddSingleton<ModelsGenerator>();
             services.AddSingleton<TemplatesRepository>();
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
 
             services.AddAuthentication();
         }
